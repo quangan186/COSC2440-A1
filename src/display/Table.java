b4099ad03c1479f31439ba4ef96a8809f2a66e76
Bui Quang An <quangan186@gmail.com>
2022-03-26 23:34:08 +0700

    student service + create Table class

---
+++ src/display/Table.java
@@ -0,0 +1,29 @@
+package display;
+
+public class Table {
+    private String[] labels;
+    private String[] rows;
+    public void display(){
+
+    }
+
+    private int[] columnLength(){
+        return new int[0];
+    }
+
+    private int width(){
+        return 0;
+    }
+
+    private void addRow(String row){
+
+    }
+
+    private void tableRow(String[] rows){
+
+    }
+
+    private void tableBorder(){
+
+    }
+}
--- src/service/StudentService.java
+++ src/service/StudentService.java
@@ -1,28 +1,35 @@
-//package service;
-//
-//import model.Course;
-//import model.Student;
-//import repo.StudentEnrolmentManager;
-//
-//import java.util.ArrayList;
-//
-//public class StudentService {
-//    private StudentEnrolmentManager sem;
-//
-//    public StudentService(StudentEnrolmentManager sem) {
-//        this.sem = sem;
-//    }
-//
-//    public void displayOnTable(ArrayList<Student> studentList){
-//
-//    }
-//
-//    public ArrayList<Student> getAllStudents(){
-//
-//    }
-//
-//    public ArrayList<Student> getAllStudentsInOneCourse(String courseID, String sem){
-//
-//    }
-//
-//}
+package service;
+
+import model.Course;
+import model.Student;
+import model.StudentEnrolment;
+import repo.StudentEnrolmentManager;
+
+import java.util.ArrayList;
+
+public class StudentService {
+    private StudentEnrolmentManager sem;
+
+    public StudentService(StudentEnrolmentManager sem) {
+        this.sem = sem;
+    }
+
+    public void displayOnTable(ArrayList<Student> studentList){
+
+    }
+
+    public ArrayList<Student> getAllStudents(){
+        return sem.getAllStudents();
+    }
+
+    public ArrayList<Student> getAllStudentsInOneCourse(String courseID, String semester){
+        ArrayList<Student> students = new ArrayList<>();
+        for (StudentEnrolment se : sem.getAllEnrolment()){
+            if (se.getCourse().getCourseID().equals(courseID) && se.getSem().equals(semester)){
+                students.add(se.getStudent());
+            }
+        }
+        return students;
+    }
+
+}
