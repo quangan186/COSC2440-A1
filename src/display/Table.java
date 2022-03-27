f2ba7d4109ded824e0cdbab1c9d86d3f6483e9e6
Bui Quang An <quangan186@gmail.com>
2022-03-28 00:16:02 +0700

    calculate table width, height

--- src/display/Table.java
+++ src/display/Table.java
@@ -1,22 +1,46 @@
 package display;
 
+import java.util.ArrayList;
+
 public class Table {
-    private String[] labels;
-    private String[] rows;
+    private final String[] labels;
+    private final ArrayList<String[]> rows = new ArrayList<>();
+    private final String sep = " | ";
     public void display(){
 
     }
 
-    private int[] columnLength(){
-        return new int[0];
+    public Table(String[] labels) {
+        this.labels = labels;
+    }
+
+    private int[] height(){
+        int[] columnLength = new int[labels.length];
+        for (int i = 0; i < columnLength.length; i++){
+            columnLength[i] = labels[i].length();
+        }
+        for (String[] row : rows){
+            for (int i = 0; i < row.length; i++){
+                if (labels[i].length() < row[i].length()){
+                    columnLength[i] = row[i].length();
+                } else {
+                    columnLength[i] = labels[i].length();
+                }
+            }
+        }
+        return columnLength;
     }
 
     private int width(){
-        return 0;
+        int widthLength = (labels.length + 1) * sep.length();
+        for (int columnLength : height()) {
+            widthLength += columnLength;
+        }
+        return widthLength;
     }
 
-    private void addRow(String row){
-
+    private void addRow(String[] row){
+        rows.add(row);
     }
 
     private void tableRow(String[] rows){
